dc767533c20c9d1ff6ffc7822f18623954d95353
Lliril <kirya.ermalenok@mail.ru>
2022-04-08 10:08:22 +0300

    implemented share photo feature

--- app/src/main/java/com/example/space/ui/details_screen/DetailsScreenFragment.kt
+++ app/src/main/java/com/example/space/ui/details_screen/DetailsScreenFragment.kt
@@ -1,10 +1,17 @@
 package com.example.space.ui.details_screen
 
 import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.FileProvider
 import coil.load
 import com.example.space.R
 import com.example.space.databinding.FragmentDetailsScreenBinding
@@ -12,9 +19,14 @@ import com.example.space.mvi_interfaces.details_screen.DetailsScreenView
 import com.example.space.presenters.DetailsScreenPresenter
 import com.example.space.utils.URL_KEY
 import com.example.space.utils.parseImageUrl
+import com.google.android.material.snackbar.BaseTransientBottomBar.LENGTH_LONG
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 import moxy.MvpAppCompatFragment
 import moxy.ktx.moxyPresenter
+import java.io.File
+import java.io.FileOutputStream
+import java.lang.Exception
 import java.util.*
 import javax.inject.Inject
 import javax.inject.Provider
@@ -46,15 +58,40 @@ class DetailsScreenFragment() : MvpAppCompatFragment(R.layout.fragment_details_s
         binding.imageBackToMain.setOnClickListener {
             requireActivity().onBackPressed()
         }
+        binding.apply {
+            imageBackToMain.setOnClickListener { requireActivity().onBackPressed() }
+            imageShare.setOnClickListener { sharePhoto() }
+        }
         presenter.showUi(url!!)
         presenter.showOnboardingScreen()
     }
 
-    override fun showPhoto(url: String) {
-        binding.imageDetailPhoto.load(url) {
-            crossfade(true)
-            crossfade(300)
+    private fun sharePhoto() {
+        val bitmapDrawable = binding.imageDetailPhoto.drawable as BitmapDrawable
+        val bitmap = bitmapDrawable.bitmap
+        //val bitmap = Bitmap.createBitmap(binding.imageDetailPhoto.width, binding.imageDetailPhoto.height, Bitmap.Config.ARGB_8888)
+        val imageFolder = File(requireActivity().cacheDir, "image")
+        val imageUri: Uri = try {
+            imageFolder.mkdirs()
+            val file = File(imageFolder, "shared_image.png")
+            val stream = FileOutputStream(file)
+            bitmap.compress(Bitmap.CompressFormat.PNG, 95, stream)
+            stream.flush()
+            stream.close()
+            FileProvider.getUriForFile(requireContext(), "com.example.space.fileprovider", file)
+        } catch (e: Exception) {
+            Snackbar.make(requireView(), getString(R.string.share_photo_error), LENGTH_LONG).show()
+            return
         }
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type = "image/png"
+        intent.putExtra(Intent.EXTRA_STREAM, imageUri)
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+        startActivity(Intent.createChooser(intent, getString(R.string.share)))
+    }
+
+    override fun showPhoto(url: String) {
+        binding.imageDetailPhoto.load(url)
     }
 
     override fun showOnboardingScreen() {
