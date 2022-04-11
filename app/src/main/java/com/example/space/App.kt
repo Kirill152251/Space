77859f55056f0f7d0f3e05cd3cdd9cc2b1316607
Lliril <kirya.ermalenok@mail.ru>
2022-04-11 13:10:36 +0300

    implement firestore push notification from console

--- app/src/main/java/com/example/space/App.kt
+++ app/src/main/java/com/example/space/App.kt
@@ -1,7 +1,9 @@
 package com.example.space
 
 import android.app.Application
+import android.util.Log
 import com.github.terrakok.cicerone.Cicerone
+import com.google.firebase.messaging.FirebaseMessaging
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
